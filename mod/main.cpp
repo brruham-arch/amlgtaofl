e502ff5e4f8c69c480771df6e6730fb0c69afa8e
brruham <brruham@gmail.com>
2026-05-22 04:46:42 +0700

    v9: MYMOD macro, no ShowToast

--- mod/main.cpp
+++ mod/main.cpp
@@ -1,10 +1,11 @@
 #include <android/log.h>
 #include <stdio.h>
+#include <dlfcn.h>
+#include "mod/amlmod.h"
 
 #define TAG     "NPCFight"
 #define LOGFILE "/storage/emulated/0/npcfight_log.txt"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define EXPORT  __attribute__((visibility("default")))
 
 static void logf(const char* msg) {
     FILE* f = fopen(LOGFILE, "a");
@@ -12,27 +13,15 @@ static void logf(const char* msg) {
     LOGI("%s", msg);
 }
 
-struct ModInfo {
-    const char* szGUID;
-    const char* szVersion;
-    const char* szName;
-    const char* szAuthor;
-    const char* szGameGUID;
-};
+MYMOD(com.brruham.npcfight, NPCFight, 1.0, brruham)
 
-static ModInfo modinfo = {
-    "com.brruham.npcfight",
-    "1.0",
-    "NPCFight",
-    "brruham",
-    "com.rockstargames.gtasa"
-};
-
-extern "C" {
-EXPORT ModInfo* __GetModInfo() { return &modinfo; }
-EXPORT void __INeedASpecificGame() {}
-EXPORT void OnModLoad() {
+ON_MOD_PRELOAD() {
     remove(LOGFILE);
-    logf("[NPCFight] OnModLoad OK");
+    logf("[NPCFight] OnModPreLoad");
 }
+
+ON_MOD_LOAD() {
+    logf("[NPCFight] OnModLoad start");
+    // TIDAK panggil aml->ShowToast dulu
+    logf("[NPCFight] OnModLoad done");
 }

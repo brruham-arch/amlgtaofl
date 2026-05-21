4ee933568d332f2d8dcfe2250717da1597a272d1
brruham <brruham@gmail.com>
2026-05-22 04:42:40 +0700

    v8: back to basics, struct ModInfo no IAML

--- mod/main.cpp
+++ mod/main.cpp
@@ -1,7 +1,5 @@
 #include <android/log.h>
 #include <stdio.h>
-#include <dlfcn.h>
-#include <string.h>
 
 #define TAG     "NPCFight"
 #define LOGFILE "/storage/emulated/0/npcfight_log.txt"
@@ -14,44 +12,27 @@ static void logf(const char* msg) {
     LOGI("%s", msg);
 }
 
-// ModInfo dengan constructor seperti libFullscreenSplash
-class ModInfo {
-public:
-    char szGUID[64];
-    char szVersion[16];
-    char szName[64];
-    char szAuthor[32];
-
-    ModInfo(const char* name, const char* author, 
-            const char* guid, const char* version) {
-        strncpy(szName,    name,    sizeof(szName)-1);
-        strncpy(szAuthor,  author,  sizeof(szAuthor)-1);
-        strncpy(szGUID,    guid,    sizeof(szGUID)-1);
-        strncpy(szVersion, version, sizeof(szVersion)-1);
-    }
+struct ModInfo {
+    const char* szGUID;
+    const char* szVersion;
+    const char* szName;
+    const char* szAuthor;
+    const char* szGameGUID;
 };
 
-static ModInfo modinfo(
+static ModInfo modinfo = {
+    "com.brruham.npcfight",
+    "1.0",
     "NPCFight",
     "brruham",
-    "com.brruham.npcfight",
-    "1.0"
-);
+    "com.rockstargames.gtasa"
+};
 
 extern "C" {
-
 EXPORT ModInfo* __GetModInfo() { return &modinfo; }
 EXPORT void __INeedASpecificGame() {}
-
 EXPORT void OnModLoad() {
     remove(LOGFILE);
     logf("[NPCFight] OnModLoad OK");
-
-    void* hAML = dlopen("libAML.so", RTLD_NOW | RTLD_NOLOAD);
-    logf(hAML ? "[NPCFight] libAML found" : "[NPCFight] libAML NOT found");
-
-    auto getIface = hAML ? (void*(*)(const char*))dlsym(hAML, "GetInterface") : nullptr;
-    logf(getIface ? "[NPCFight] GetInterface found" : "[NPCFight] GetInterface NOT found");
 }
-
 }

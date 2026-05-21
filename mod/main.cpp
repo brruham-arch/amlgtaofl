5e103c7160f22d4c44c725eaaedd737c3fdc36ba
brruham <brruham@gmail.com>
2026-05-22 04:12:37 +0700

    v6: struct ModInfo seperti drawdistance, hapus OnModPreLoad

--- mod/main.cpp
+++ mod/main.cpp
@@ -1,5 +1,6 @@
 #include <android/log.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #define TAG     "NPCFight"
 #define LOGFILE "/storage/emulated/0/npcfight_log.txt"
@@ -12,20 +13,29 @@ static void logf(const char* msg) {
     LOGI("%s", msg);
 }
 
+struct ModInfo {
+    const char* szGUID;
+    const char* szVersion;
+    const char* szName;
+    const char* szAuthor;
+    const char* szGameGUID;
+};
+
+static ModInfo modInfo = {
+    "com.brruham.npcfight",
+    "1.0",
+    "NPCFight",
+    "brruham",
+    "com.rockstargames.gtasa"
+};
+
 extern "C" {
 
-EXPORT const char* __GetModInfo() {
-    return "com.brruham.npcfight|1.0|NPCFight|brruham";
-}
-
+EXPORT ModInfo* __GetModInfo() { return &modInfo; }
 EXPORT void __INeedASpecificGame() {}
 
-EXPORT void OnModPreLoad() {
-    remove(LOGFILE);
-    logf("[NPCFight] OnModPreLoad OK");
-}
-
 EXPORT void OnModLoad() {
+    remove(LOGFILE);
     logf("[NPCFight] OnModLoad OK");
 }
 
